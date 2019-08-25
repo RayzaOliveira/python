b1f7ab61c9a955aba0d7ffeff71ae2c3e44c5f99
Rayza Oliveira <rayza.ocr@gmail.com>
2019-08-24 22:08:49 -0300

    My first commit

---
+++ comando if.py
@@ -0,0 +1,35 @@
+'''  if condição:
+     execute_esta_linha.
+
+     bloco: trecho de código q depende de outro trecho. '''
+
+# -*- coding: utf-8 -*- 
+
+x=1
+y=10000000000
+
+if x>y:
+	print ("x é maior q y!")
+
+if y>x:
+	print ("y é maior q x!")
+
+# Indentação : tecla tab q gera espaço. 
+
+# Comando else: é executado caso o camando de if não seja atendida.
+
+
+x=1 
+y=2 
+
+if x>y:
+	print("x é maior!")
+else:
+	print ("y é maior!")
+
+
+# Comando elif : caso seja necessário mais condições entre if e else. 
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,79 @@
+print ("hello word!")  
+#nao colcar acentos pq tode ocorrer ERRO 
+print ("outra messagem")
+
+#cometario 
+
+"""comentario
+ de 
+muitas 
+linhas """
+
+# codigo para colocar acento:
+# -*- coding: utf-8 -*-
+
+print ("Olá mundo!")
+
+print (2+2)
+#espoente: 
+print (2**3)
+#modulo retorna resto da divisão:
+print (10 % 3)
+
+#variavel
+#nome_da_variavel=valor
+#nao pode ter espaço, entao usa anderlaine _
+
+
+minha_variavel="olá mundo!"
+
+print (minha_variavel)
+minha_variavel+="!"
+print (minha_variavel)
+minha_variavel+="!"
+print (minha_variavel)
+
+var1 = 1 #variável inteira int
+var2 = 1.1 #variável float
+var3 = "Eu sou uma string" #variável string 
+
+var4 = True # verdadeiro , booleana
+var4 = False #falso , booleana
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+#operadores relacionais:
+
+# == igual
+# != diferente 
+# > maior
+# < menor 
+# >= maior ou igual 
+# <= menor ou igual  
+
+
+x = 3
+y = 3
+z = 3
+
+soma = x + y 
+
+print (soma >= y)
+print (soma == x)
+
+#operdadores logicos:
+
+# and duas condiçoes sejam verdadeiras 
+# or pelo menos uma condiçao seja verdadeira 
+# not Inverte o valor 
+
+
+print (x==y or x==z and z==y)
+
+
+
+
+
